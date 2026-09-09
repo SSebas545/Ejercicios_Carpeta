f5a58a8b2db80360909c6766f6b47258ec1850d7
Sebastian Rivero <sebasdrm3010@gmail.com>
2026-09-09 10:28:22 -0300

    Update Vectores_Carpeta_Ej6.c

--- Vectores_Carpeta_Ej6.c
+++ Vectores_Carpeta_Ej6.c
@@ -3,17 +3,23 @@
 int main() 
 {
     int limite, i;
-    int fibonacci[2] = {0, 1};
+    
     printf("Ingrese el limite para la sucesion de Fibonacci: ");
     scanf("%d", &limite);
-    for (i = 2; i < limite; i++) 
-    {
+    
+    int fibonacci[limite];
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    
+    for (i = 2; i < limite; i++) {
         fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
     }
+    
     printf("La sucesion de Fibonacci hasta %d es: ", limite);
-    for (i = 0; i < limite; i++) 
-    {
+    for (i = 0; i < limite; i++) {
         printf("%d ", fibonacci[i]);
     }
+    printf("\n");
+    
     return 0;
 }
